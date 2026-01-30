fb69ebea51b4ea092f232bc834a3c57c16fa9497
Recasted <lijkvocht1@gmail.com>
2026-01-30 04:16:32 +0100

    Refactor logo click event and add hamburger menu

--- script.js
+++ script.js
@@ -31,3 +31,11 @@ logo.addEventListener('click', function(e) {
 
   requestAnimationFrame(scrollStep);
 });
+
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
